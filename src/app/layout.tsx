233a1bdf9b8886345f4789a0144778d8bee7ff93
LugpDev2022 <lugpdev2022@gmail.com>
2024-05-29 11:56:40 -0600

    Move fonts to app dir

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 const NetflixSans = localFont({
   src: [
     {
-      path: '../fonts/netflix-sans.otf',
+      path: './fonts/netflix-sans.otf',
       weight: '400',
       style: 'normal',
     },
