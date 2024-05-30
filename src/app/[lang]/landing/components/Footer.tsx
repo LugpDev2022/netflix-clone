8f6e1e29d641af460fc032322667d42f4de78684
LugpDev2022 <lugpdev2022@gmail.com>
2024-05-30 13:32:00 -0600

    Create a simple footer

---
+++ src/app/[lang]/landing/components/Footer.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <footer className='bg-black text-center py-3'>
+      This website is not the official Netflix site. It was created by{' '}
+      <a
+        href='https://github.com/LugpDev2022'
+        target='_blank'
+        rel='noopener noreferrer'
+        className='font-bold'
+      >
+        @LugpDev2022{' '}
+      </a>
+      for programming practice.
+    </footer>
+  );
+};
+
+export default Footer;
--- src/app/[lang]/landing/page.tsx
+++ src/app/[lang]/landing/page.tsx
@@ -2,6 +2,7 @@ import FirstSection from '@/src/app/[lang]/landing/sections/FirstSection';
 import SecondSection from '@/src/app/[lang]/landing/sections/SecondSection';
 import ThirdSection from '@/src/app/[lang]/landing/sections/ThirdSection';
 import FourthSection from '@/src/app/[lang]/landing/sections/FourthSection';
+import Footer from '@/src/app/[lang]/landing/components/Footer';
 import { Locale } from '@/src/types';
 import './landing.css';
 
@@ -18,6 +19,7 @@ const LandingPage: React.FC<Props> = async ({ params: { lang } }) => {
       <SecondSection lang={lang} />
       <ThirdSection lang={lang} />
       <FourthSection lang={lang} />
+      <Footer />
     </div>
   );
 };
