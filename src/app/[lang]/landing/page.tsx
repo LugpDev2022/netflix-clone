04e8c9d3a42bf91cf597a52554784b2e831c4f56
LugpDev2022 <lugpdev2022@gmail.com>
2024-05-30 11:14:31 -0600

    Structures and style for the third landing page section

--- src/app/[lang]/landing/page.tsx
+++ src/app/[lang]/landing/page.tsx
@@ -1,5 +1,6 @@
 import FirstSection from '@/src/app/[lang]/landing/sections/FirstSection';
 import SecondSection from '@/src/app/[lang]/landing/sections/SecondSection';
+import ThirdSection from '@/src/app/[lang]/landing/sections/ThirdSection';
 import { Locale } from '@/src/types';
 import './landing.css';
 
@@ -14,6 +15,7 @@ const LandingPage: React.FC<Props> = async ({ params: { lang } }) => {
     <div className='bg-gray-700'>
       <FirstSection lang={lang} />
       <SecondSection lang={lang} />
+      <ThirdSection lang={lang} />
     </div>
   );
 };
---
+++ src/app/[lang]/landing/sections/ThirdSection.tsx
@@ -0,0 +1,37 @@
+import { getDictionary } from '@/src/app/[lang]/dictionaries';
+import { Locale } from '@/src/types';
+import Image from 'next/image';
+
+interface Props {
+  lang: Locale;
+}
+
+const ThirdSection: React.FC<Props> = ({ lang }) => {
+  return (
+    <section className='landing-section-container'>
+      <div className='landing-section'>
+        <div className='landing-img-container'>
+          <Image src='/device-pile.png' alt='TV' width={530} height={400} />
+          <video
+            src='/video-devices.m4v'
+            className='top-[7%] w-[62%] left-1/2 -translate-x-1/2'
+            loop
+            autoPlay
+            playsInline
+            muted
+          ></video>
+        </div>
+
+        <div>
+          <h2>Watch everywhere</h2>
+          <p>
+            Stream unlimited movies and TV shows on your phone, tablet, laptop,
+            and TV.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ThirdSection;
