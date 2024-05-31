98bb295551d7d7b43987bbcaa6a19b5d0af34bb8
LugpDev2022 <lugpdev2022@gmail.com>
2024-05-31 10:58:45 -0600

    Style the register form input

--- src/app/[lang]/landing/components/RegisterForm.tsx
+++ src/app/[lang]/landing/components/RegisterForm.tsx
@@ -3,10 +3,13 @@
 import { Formik } from 'formik';
 import { GrNext } from 'react-icons/gr';
 
+import '../register-form.css';
+
 interface Props {
   dictionary: any;
 }
 
+//TODO: Finish register form
 const RegisterForm: React.FC<Props> = ({ dictionary }) => {
   return (
     <Formik
@@ -17,11 +20,19 @@ const RegisterForm: React.FC<Props> = ({ dictionary }) => {
     >
       {() => (
         <form className='mt-4 flex flex-wrap gap-4 justify-center max-w-[600px] mx-auto'>
-          <input
-            type='email'
-            name='email'
-            className='bg-black/80 w-full border border-gray-400 outline-none rounded-[4px] h-12 sm:w-auto sm:grow'
-          />
+          <div className='relative w-full sm:w-auto sm:grow'>
+            <input
+              type='email'
+              name='email'
+              id='email'
+              className='register-form-input'
+              placeholder=' '
+              required
+            />
+            <label htmlFor='email' className='register-form-label'>
+              Email
+            </label>
+          </div>
           <button
             type='submit'
             className='bg-red-600 hover:bg-red-700 flex gap-2 text-lg font-medium items-center px-4 py-2 rounded-[4px] sm:text-2xl'
