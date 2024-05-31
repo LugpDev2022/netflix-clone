5e6878fae1eff94ac822f36ca433d981894d415e
LugpDev2022 <lugpdev2022@gmail.com>
2024-05-30 22:13:01 -0600

    Add contact info on landing page footer

--- src/app/[lang]/landing/components/Footer.tsx
+++ src/app/[lang]/landing/components/Footer.tsx
@@ -1,5 +1,8 @@
+import { FaGithub, FaLinkedin, FaSquareXTwitter } from 'react-icons/fa6';
+
 import { getDictionary } from '@/src/app/[lang]/dictionaries';
 import { Locale } from '@/src/types';
+import LangSelector from './LangSelector';
 interface Props {
   lang: Locale;
 }
@@ -10,17 +13,45 @@ const Footer: React.FC<Props> = async ({ lang }) => {
   return (
     <footer className='bg-black text-left py-3'>
       <div className='max-w-screen-lg mx-auto'>
-        <p>This website is not the official Netflix site.</p>
-        It was created by{' '}
-        <a
-          href='https://github.com/LugpDev2022'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='font-bold'
-        >
-          @LugpDev2022{' '}
-        </a>
-        for programming practice.
+        <h4>Important Information</h4>
+        <p>
+          This website is not the official Netflix site. It was created by Luis
+          Cervantes for programming practice.
+        </p>
+
+        <h4>Contact</h4>
+        <ul>
+          <li>
+            <FaGithub />
+            <a
+              href='https://github.com/LugpDev2022'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              @LugpDev2022
+            </a>
+          </li>
+          <li>
+            <FaLinkedin />
+            <a
+              href='https://www.linkedin.com/in/luiscerv/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              in/luiscerv
+            </a>
+          </li>
+          <li>
+            <FaSquareXTwitter />
+            <a
+              href='https://twitter.com/LugpDev2022'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              @LugpDev2022
+            </a>
+          </li>
+        </ul>
       </div>
     </footer>
   );
