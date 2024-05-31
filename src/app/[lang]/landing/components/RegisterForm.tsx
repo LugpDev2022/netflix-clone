004d53f5bc69be0597c914ff7d1bc6c04995d19f
LugpDev2022 <lugpdev2022@gmail.com>
2024-05-31 12:01:10 -0600

    Validate register form

--- src/app/[lang]/landing/components/RegisterForm.tsx
+++ src/app/[lang]/landing/components/RegisterForm.tsx
@@ -1,14 +1,19 @@
 'use client';
 
-import { Form, Formik } from 'formik';
+import { Field, Form, Formik } from 'formik';
 import { GrNext } from 'react-icons/gr';
 import { RxCrossCircled } from 'react-icons/rx';
+import * as Yup from 'yup';
 
 import '../register-form.css';
 
 interface Props {
   dictionary: any;
 }
+
+const registerSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid Email').required('Email is required'),
+});
 
 //TODO: Finish register form
 const RegisterForm: React.FC<Props> = ({ dictionary }) => {
@@ -17,32 +22,39 @@ const RegisterForm: React.FC<Props> = ({ dictionary }) => {
       initialValues={{
         email: '',
       }}
-      initialErrors={{
-        email: 'Escribe una dirección de email válida',
-      }}
+      validationSchema={registerSchema}
+      validateOnBlur
+      validateOnChange
       onSubmit={() => {}}
     >
       {({ errors }) => (
         <Form className='register-form'>
           <div className='relative w-full sm:w-auto sm:grow'>
-            <input
-              type='email'
+            <Field
               name='email'
+              type='email'
               id='email'
               className={`register-form-input ${
-                errors ? 'register-form-input-error' : ''
+                errors.email ? 'register-form-input-error' : ''
               }`}
               placeholder=' '
               required
             />
+
             <label htmlFor='email' className='register-form-label'>
               Email
             </label>
           </div>
-          <span className='register-form-error-message'>
-            <RxCrossCircled size={18} />
-            {errors.email}
-          </span>
+
+          {errors.email ? (
+            <span className='register-form-error-message'>
+              <RxCrossCircled size={18} />
+              {errors.email}
+            </span>
+          ) : (
+            <></>
+          )}
+
           <button type='submit' className='register-form-submit-btn'>
             {dictionary.landing.registerButton} <GrNext size={22} />
           </button>
