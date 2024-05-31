6b1b8b170539ab7947a2dbcaf5d029c29224b530
LugpDev2022 <lugpdev2022@gmail.com>
2024-05-31 11:33:30 -0600

    Transfer most of register form styles to vanilla css

--- src/app/[lang]/landing/components/RegisterForm.tsx
+++ src/app/[lang]/landing/components/RegisterForm.tsx
@@ -23,7 +23,7 @@ const RegisterForm: React.FC<Props> = ({ dictionary }) => {
       onSubmit={() => {}}
     >
       {({ errors }) => (
-        <Form className='mt-4 flex flex-wrap gap-6 justify-center max-w-[600px] mx-auto relative'>
+        <Form className='register-form'>
           <div className='relative w-full sm:w-auto sm:grow'>
             <input
               type='email'
@@ -39,14 +39,11 @@ const RegisterForm: React.FC<Props> = ({ dictionary }) => {
               Email
             </label>
           </div>
-          <span className='w-full text-left text-[#eb3942] flex gap-1.5 items-center text-sm absolute top-[52px]'>
+          <span className='register-form-error-message'>
             <RxCrossCircled size={18} />
             {errors.email}
           </span>
-          <button
-            type='submit'
-            className='bg-red-600 hover:bg-red-700 flex gap-2 text-lg font-medium items-center px-4 py-2 rounded-[4px] sm:text-2xl'
-          >
+          <button type='submit' className='register-form-submit-btn'>
             {dictionary.landing.registerButton} <GrNext size={22} />
           </button>
         </Form>
