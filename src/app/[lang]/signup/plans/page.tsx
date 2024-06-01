5cc3bb4b91147aa6c637bd32423823965b48e9b2
LugpDev2022 <lugpdev2022@gmail.com>
2024-05-31 23:14:40 -0600

    Create the basic structure of the plans page

--- src/app/[lang]/signup/plans/page.tsx
+++ src/app/[lang]/signup/plans/page.tsx
@@ -13,10 +13,23 @@ const PlansPage: React.FC<Props> = async ({ params: { lang } }) => {
   return (
     <main className='sign-up-main max-w-[700px]'>
       <span className='step-indicator'>
-        {dict.signUp.step} <strong>1</strong> {dict.signUp.of}{' '}
+        {dict.signUp.step} <strong>2</strong> {dict.signUp.of}{' '}
         <strong>3</strong>
       </span>
       <h1 className='step-title'>Choose the plan that’s right for you</h1>
+
+      <div className='grid grid-cols-3 gap-2 lg:gap-3'>
+        <div className='bg-blue-300'>Premium</div>
+        <div className='bg-blue-300'>Standard</div>
+        <div className='bg-blue-300'>Standard with ads</div>
+      </div>
+
+      <ul>
+        <li>
+          <h2>Monthly Price</h2>
+          <p>MX$299</p>
+        </li>
+      </ul>
     </main>
   );
 };
