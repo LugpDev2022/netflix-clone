3fe69ce594b9cc9b405d47094469c4ca5dd66f50
LugpDev2022 <lugpdev2022@gmail.com>
2024-06-01 16:57:36 -0600

    Change between plans functionality

--- src/app/[lang]/signup/plans/components/PlanSelector.tsx
+++ src/app/[lang]/signup/plans/components/PlanSelector.tsx
@@ -1,39 +1,63 @@
+'use client';
+
 import { FaCheckCircle } from 'react-icons/fa';
 
-import data from '../plans.json';
 import { Locale } from '@/src/types';
+import data from '../plans.json';
+import { useState } from 'react';
 
 interface Props {
   lang: Locale;
 }
 
+type Plan = 'premium' | 'standard' | 'standard_with_ads';
+
 const PlanSelector: React.FC<Props> = ({ lang }) => {
+  const [plan, setPlan] = useState<Plan>('premium');
+
   return (
     <>
       <div className='grid grid-cols-3 gap-2 lg:gap-3'>
         {/* TODO: Add background radiants */}
-        <div className='plan-card-selected bg-blue-600'>
+        <button
+          onClick={() => setPlan('premium')}
+          className={
+            plan === 'premium' ? 'plan-card-selected bg-blue-600' : 'plan-card'
+          }
+        >
           <h2>Premium</h2>
           <sub>4k + HDR</sub>
 
           <FaCheckCircle />
-        </div>
-        <div className='plan-card'>
+        </button>
+        <button
+          onClick={() => setPlan('standard')}
+          className={
+            plan === 'standard' ? 'plan-card-selected bg-blue-600' : 'plan-card'
+          }
+        >
           <h2>Standard</h2>
           <sub>1080p</sub>
 
           <FaCheckCircle />
-        </div>
-        <div className='plan-card'>
+        </button>
+        <button
+          onClick={() => setPlan('standard_with_ads')}
+          className={
+            plan === 'standard_with_ads'
+              ? 'plan-card-selected bg-blue-600'
+              : 'plan-card'
+          }
+        >
           <h2>Standard with ads</h2>
           <sub>1080p</sub>
 
           <FaCheckCircle />
-        </div>
+        </button>
       </div>
 
       <ul className='plan-feature-list'>
-        {data[lang].standard_with_ads.map(({ label, value }) => (
+        {data[lang][plan].map(({ label, value }) => (
           <li key={label}>
             <h3>{label}</h3>
             <p>{value}</p>
