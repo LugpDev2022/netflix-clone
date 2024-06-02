0cfbe2d64772039daff8640eb38d01dfc2ed2dbb
LugpDev2022 <lugpdev2022@gmail.com>
2024-06-01 18:52:57 -0600

    Create the sign up data state

--- src/app/[lang]/signup/context/DataContextProvider.tsx
+++ src/app/[lang]/signup/context/DataContextProvider.tsx
@@ -1,12 +1,28 @@
 'use client';
 
+import { useState } from 'react';
 import { DataContext } from './DataContext';
+import { Plan } from '../types';
 
 interface Props {
   children: React.ReactNode;
 }
 
+interface StateValue {
+  email: string | null;
+  password: string | null;
+  plan: Plan | null;
+  price: number | null;
+}
+
 const DataContextProvider: React.FC<Props> = ({ children }) => {
+  const [data, setData] = useState<StateValue>({
+    email: null,
+    password: null,
+    plan: null,
+    price: 0,
+  });
+
   return <DataContext.Provider value={null}>{children}</DataContext.Provider>;
 };
 
