49f5709717826d2805fd58046f798b9c7363cd02
LugpDev2022 <lugpdev2022@gmail.com>
2024-06-11 10:38:21 -0600

    Add login a background image to md screens

--- src/app/[lang]/login/page.tsx
+++ src/app/[lang]/login/page.tsx
@@ -6,29 +6,31 @@ import './login.css';
 
 const LoginPage = () => {
   return (
-    <div className='bg-black text-white min-h-screen'>
-      <header className='p-6'>
-        <Image
-          src='/logo.svg'
-          width={148}
-          height={40}
-          alt='Netflix'
-          className='w-[89px] h-6'
-        />
-      </header>
+    <div className='login-bg text-white'>
+      <div className='bg-gradient-to-b from-black/80 via-black/50 to-black/80 min-h-screen'>
+        <header className='p-6'>
+          <Image
+            src='/logo.svg'
+            width={148}
+            height={40}
+            alt='Netflix'
+            className='w-[89px] h-6'
+          />
+        </header>
 
-      <main className='px-6'>
-        <h1 className='text-[32px] font-bold'>Sign In</h1>
+        <main className='px-6'>
+          <h1 className='text-[32px] font-bold'>Sign In</h1>
 
-        <SignInForm />
+          <SignInForm />
 
-        <p className='text-[#ffffffb3] mt-4'>
-          New to Netflix?{' '}
-          <Link href='/signup' className='text-white'>
-            Sign up now.
-          </Link>
-        </p>
-      </main>
+          <p className='text-[#ffffffb3] mt-4'>
+            New to Netflix?{' '}
+            <Link href='/signup' className='text-white'>
+              Sign up now.
+            </Link>
+          </p>
+        </main>
+      </div>
     </div>
   );
 };
