c4107f085b38a6c8f2190301060ca68af4501f0f
LugpDev2022 <lugpdev2022@gmail.com>
2024-06-11 08:00:25 -0600

    Configure sign up middleware

--- src/middlewares/signUp.ts
+++ src/middlewares/signUp.ts
@@ -1,6 +1,39 @@
+import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
 export function signUpMiddleware(request: NextRequest) {
-  console.log('sign up');
+  const cookiesStore = cookies();
+  const cookie = cookiesStore.get('sign-up-cookie');
+
+  if (!cookie) {
+    if (request.nextUrl.pathname.endsWith('/signup')) {
+      return NextResponse.next();
+    }
+
+    return NextResponse.redirect(new URL('/signup', request.url));
+  }
+
+  const { email, password, plan, price } = JSON.parse(cookie.value);
+
+  if (!email || !password) {
+    if (request.nextUrl.pathname.endsWith('/signup')) {
+      return NextResponse.next();
+    }
+
+    return NextResponse.redirect(new URL('/signup', request.url));
+  }
+
+  if (!plan || !price) {
+    if (request.nextUrl.pathname.endsWith('/signup/plans')) {
+      return NextResponse.next();
+    }
+
+    return NextResponse.redirect(new URL('/signup/plans', request.url));
+  }
+
+  if (!request.nextUrl.pathname.endsWith('/signup/confirmation')) {
+    return NextResponse.redirect(new URL('/signup/confirmation', request.url));
+  }
+
   return NextResponse.next();
 }
