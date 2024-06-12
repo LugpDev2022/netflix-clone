6a1278559369343911311be0100534fd2b83212a
LugpDev2022 <lugpdev2022@gmail.com>
2024-06-11 18:36:37 -0600

    Create the credentials provider

--- src/app/auth/lib/authOptions.ts
+++ src/app/auth/lib/authOptions.ts
@@ -1,8 +1,21 @@
 import { AuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 export const authOptions: AuthOptions = {
   session: {
     strategy: 'jwt',
   },
-  providers: [],
+  providers: [
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {},
+
+      authorize: async (credentials, req) => {
+        return {
+          id: '',
+          email: '123@gmail.com',
+        };
+      },
+    }),
+  ],
 };
