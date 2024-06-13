368c71c3bfb2aa9993f642164aeb40e033eeb17e
LugpDev2022 <lugpdev2022@gmail.com>
2024-06-13 11:46:08 -0600

    Transfer generic footer css styles to globals.css

--- src/app/[lang]/(not-protected)/components/GenericFooter.tsx
+++ src/app/[lang]/(not-protected)/components/GenericFooter.tsx
@@ -20,77 +20,64 @@ const GenericFooter: React.FC<Props> = async ({ lang, theme }) => {
 
   return (
     <footer
-      className={` 
-      p-6
-      text-center
-      lg:text-left
-      ${
-        theme === 'light'
-          ? 'bg-[#f3f3f3] text-[#737373] border-t border-[#e6e6e6]'
-          : 'bg-black text-white/70'
-      }`}
+      className={
+        theme === 'light' ? 'footer-container-light' : 'footer-container-dark'
+      }
     >
       <div>
-        <h4 className='text-lg font-bold'>{dict.genericFooter.subtitle1}</h4>
+        <h4 className='footer-subtitle'>{dict.genericFooter.subtitle1}</h4>
         <p className='text-pretty'>{dict.genericFooter.information}</p>
 
-        <h4 className='text-lg font-bold mt-6'>
-          {dict.genericFooter.subtitle2}
-        </h4>
+        <h4 className='footer-subtitle'>{dict.genericFooter.subtitle2}</h4>
         <ul className='mt-4 grid grid-cols-1 justify-center gap-5 sm:gap-4 sm:grid-cols-2 md:grid-cols-4'>
-          <li className='flex gap-2.5 justify-center lg:justify-start'>
-            <FaGithub size={24} />
+          <li className='footer-contact-list-item'>
             <a
               href='https://github.com/LugpDev2022'
               target='_blank'
               rel='noopener noreferrer'
-              className='hover:underline'
             >
-              @LugpDev2022
+              <FaGithub size={24} />
+              <span>@LugpDev2022</span>
             </a>
           </li>
-          <li className='flex gap-2.5 justify-center lg:justify-start'>
-            <FaLinkedin size={24} />
+          <li className='footer-contact-list-item'>
             <a
               href={`https://www.linkedin.com/in/luiscerv${
                 lang === 'en' ? '/en' : '/'
               }`}
               target='_blank'
               rel='noopener noreferrer'
-              className='hover:underline'
             >
-              in/luiscerv
+              <FaLinkedin size={24} />
+              <span>in/luiscerv</span>
             </a>
           </li>
-          <li className='flex gap-2.5 justify-center lg:justify-start'>
-            <FaSquareXTwitter size={24} />
+          <li className='footer-contact-list-item'>
             <a
               href='https://twitter.com/LugpDev2022'
               target='_blank'
               rel='noopener noreferrer'
               className='hover:underline'
             >
-              @LugpDev2022
+              <FaSquareXTwitter size={24} />
+              <span>@LugpDev2022</span>
             </a>
           </li>
-          <li className='flex gap-2.5 justify-center lg:justify-start'>
-            <FaLink size={24} />
+          <li className='footer-contact-list-item'>
             <a
               href={`https://luiscerv.tech${lang === 'en' ? '/' : '/es'}`}
               target='_blank'
               rel='noopener noreferrer'
               className='hover:underline'
             >
-              luiscerv.tech
+              <FaLink size={24} />
+              <span>luiscerv.tech</span>
             </a>
           </li>
         </ul>
 
-        {/* TODO: Add a lang switch */}
         <div className='mt-6'>
-          <h4 className='text-lg font-bold mb-2'>
-            {dict.genericFooter.subtitle3}
-          </h4>
+          <h4 className='footer-subtitle'>{dict.genericFooter.subtitle3}</h4>
           <LangSelector lang={lang} theme={theme} />
         </div>
       </div>
