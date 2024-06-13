3b7ea56e17e49aa0392200d25a99703e62e90772
LugpDev2022 <lugpdev2022@gmail.com>
2024-06-13 10:16:34 -0600

    Create the generic footer structure

--- src/app/[lang]/(not-protected)/components/GenericFooter.tsx
+++ src/app/[lang]/(not-protected)/components/GenericFooter.tsx
@@ -1,3 +1,10 @@
+import {
+  FaGithub,
+  FaLinkedin,
+  FaSquareXTwitter,
+  FaLink,
+} from 'react-icons/fa6';
+
 import { getDictionary } from '@/src/app/[lang]/dictionaries';
 import { Locale } from '@/src/types';
 
@@ -9,7 +16,63 @@ interface Props {
 const GenericFooter: React.FC<Props> = async ({ lang, className = '' }) => {
   const dict = await getDictionary(lang);
 
-  return <footer className='bg-red-600'>Footer</footer>;
+  return (
+    <footer className='bg-red-600'>
+      {/* TODO: Set custom max width */}
+      <div>
+        <h4 className=''>{dict.landing.footer.subtitle1}</h4>
+        <p className='text-pretty'>{dict.landing.footer.information}</p>
+
+        <h4 className='mt-6'>{dict.landing.footer.subtitle2}</h4>
+        <ul className='mt-4 grid grid-cols-1 justify-center sm:grid-cols-2 md:grid-cols-4 gap-4'>
+          <li className=''>
+            <FaGithub />
+            <a
+              href='https://github.com/LugpDev2022'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              @LugpDev2022
+            </a>
+          </li>
+          <li className=''>
+            <FaLinkedin />
+            <a
+              href={`https://www.linkedin.com/in/luiscerv${
+                lang === 'en' ? '/en' : '/'
+              }`}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              in/luiscerv
+            </a>
+          </li>
+          <li className=''>
+            <FaSquareXTwitter />
+            <a
+              href='https://twitter.com/LugpDev2022'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              @LugpDev2022
+            </a>
+          </li>
+          <li className=''>
+            <FaLink />
+            <a
+              href={`https://luiscerv.tech${lang === 'en' ? '/' : '/es'}`}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              luiscerv.tech
+            </a>
+          </li>
+        </ul>
+
+        {/* TODO: Add a lang switch */}
+      </div>
+    </footer>
+  );
 };
 
 export default GenericFooter;
