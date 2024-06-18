748aef74fbeed79793aad9f973932d57ce494dab
LugpDev2022 <lugpdev2022@gmail.com>
2024-06-18 11:57:26 -0600

    Create the app navbar structure

--- src/app/[lang]/(protected)/components/Navbar.tsx
+++ src/app/[lang]/(protected)/components/Navbar.tsx
@@ -1,8 +1,35 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { HiMagnifyingGlass } from 'react-icons/hi2';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header>
+      <Image src='/logo.svg' alt='Netflix' width={89} height={24} />
 
-export default Navbar
+      <nav>
+        <ul>
+          <li>
+            <Link href='/'>Home</Link>
+          </li>
+          <li>
+            <Link href='/series'>Series</Link>
+          </li>
+          <li>
+            <Link href='/movies'>Movies</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div>
+        <Link href='/search'>
+          <HiMagnifyingGlass />
+        </Link>
+        <Image src='/avatar.png' alt='Avatar' width={50} height={50} />
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
