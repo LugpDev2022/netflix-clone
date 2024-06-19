f99cdc0bae4c178f50b43e48f48c0213dd15c51f
LugpDev2022 <lugpdev2022@gmail.com>
2024-06-18 18:52:52 -0600

    Add the active class to the active nav item on app navbar

--- src/app/[lang]/(protected)/components/Navbar.tsx
+++ src/app/[lang]/(protected)/components/Navbar.tsx
@@ -1,22 +1,41 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import { HiMagnifyingGlass } from 'react-icons/hi2';
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
     <header className='grid grid-cols-2 p-5 items-center bg-black text-white/80 fixed w-full sm:flex sm:justify-between'>
       <Image src='/logo.svg' alt='Netflix' width={125} height={34} />
 
       <nav className='col-span-2 order-2 mt-5 sm:order-1 sm:mt-0'>
         <ul className='flex justify-center gap-7 font-semibold'>
-          <li className='nav-item-active'>
+          <li
+            className={
+              pathname === '/en' || pathname === '/es'
+                ? 'nav-item-active'
+                : 'nav-item'
+            }
+          >
             <Link href='/'>Home</Link>
           </li>
-          <li className='nav-item'>
+          <li
+            className={
+              pathname.includes('/series') ? 'nav-item-active' : 'nav-item'
+            }
+          >
             <Link href='/series'>Series</Link>
           </li>
-          <li className='nav-item'>
+          <li
+            className={
+              pathname.includes('/movies') ? 'nav-item-active' : 'nav-item'
+            }
+          >
             <Link href='/movies'>Movies</Link>
           </li>
         </ul>
