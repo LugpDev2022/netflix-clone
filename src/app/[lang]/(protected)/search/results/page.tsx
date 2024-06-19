c70e08fa8eb1c75a129d113ae02464e8db4a008f
LugpDev2022 <lugpdev2022@gmail.com>
2024-06-19 16:00:26 -0600

    Fetch TMDB on search request

--- src/app/[lang]/(protected)/search/results/page.tsx
+++ src/app/[lang]/(protected)/search/results/page.tsx
@@ -1,17 +1,37 @@
+import { Locale } from '@/src/types';
 import { redirect } from 'next/navigation';
 
 interface Props {
   searchParams: {
     q: string;
   };
+  params: {
+    lang: Locale;
+  };
 }
 
-const ResultsPage: React.FC<Props> = ({ searchParams: { q: query } }) => {
+const ResultsPage: React.FC<Props> = async ({
+  params: { lang },
+  searchParams: { q: query },
+}) => {
   if (!query) return redirect('/search');
 
-  console.log(query);
+  const resp = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&language=${lang}&query=${query}`
+  );
 
-  return <div>ResultsPage</div>;
+  const data = await resp.json();
+
+  const { results } = data;
+
+  return (
+    <div>
+      {results.map(({ key, title }: any) => (
+        // TODO: Create a card
+        <div key={key}>{title}</div>
+      ))}
+    </div>
+  );
 };
 
 export default ResultsPage;
