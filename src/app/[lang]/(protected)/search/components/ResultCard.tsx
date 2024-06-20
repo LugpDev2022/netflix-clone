0ed3caf93d3559f11e5c7317cf8812a1d9e21400
LugpDev2022 <lugpdev2022@gmail.com>
2024-06-20 10:05:23 -0600

    Configure next image to work with TMDB images

--- src/app/[lang]/(protected)/search/components/ResultCard.tsx
+++ src/app/[lang]/(protected)/search/components/ResultCard.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 interface Props {
   image: string;
   title: string;
@@ -7,7 +9,7 @@ interface Props {
 const ResultCard: React.FC<Props> = ({ image, releaseYear, title }) => {
   return (
     <article>
-      <img src={image} alt={`${title} Poster`} />
+      <Image src={image} width={335} height={189} alt={`${title} Poster`} />
 
       <div>
         <h2>{title}</h2>
