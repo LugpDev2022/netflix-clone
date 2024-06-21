897a20d8273e37144b04375f3ced25ffb2c98fde
LugpDev2022 <lugpdev2022@gmail.com>
2024-06-21 12:05:14 -0600

    Translate the search page

--- src/app/[lang]/(protected)/search/page.tsx
+++ src/app/[lang]/(protected)/search/page.tsx
@@ -3,6 +3,7 @@ import { getPopularMovies } from './lib/getPopularMovies';
 import { getPopularSeries } from './lib/getPopularSeries';
 import Card from './components/Card';
 import './search.css';
+import { getDictionary } from '../../dictionaries';
 
 interface Props {
   params: {
@@ -11,12 +12,14 @@ interface Props {
 }
 
 const SearchPage: React.FC<Props> = async ({ params: { lang } }) => {
+  const dict = await getDictionary(lang);
+
   const [moviesErr, movies] = await getPopularMovies(lang);
   const [seriesErr, series] = await getPopularSeries(lang);
 
   return (
     <>
-      <h2 className='category-subtitle'>Popular movies</h2>
+      <h2 className='category-subtitle'>{dict.app.searchPage.popularMovies}</h2>
       <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5  gap-2 lg:gap-2.5'>
         {movies?.map((movie: any) => (
           <Card
@@ -28,7 +31,7 @@ const SearchPage: React.FC<Props> = async ({ params: { lang } }) => {
         ))}
       </div>
 
-      <h2 className='category-subtitle'>Popular Series</h2>
+      <h2 className='category-subtitle'>{dict.app.searchPage.popularSeries}</h2>
       <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5  gap-2 lg:gap-2.5'>
         {series?.map((show: any) => (
           <Card
