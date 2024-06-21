d5bc97883f973d5e3b8772815009a58a6e9eec6a
LugpDev2022 <lugpdev2022@gmail.com>
2024-06-21 11:22:53 -0600

    Get popular series on search page

--- src/app/[lang]/(protected)/search/page.tsx
+++ src/app/[lang]/(protected)/search/page.tsx
@@ -1,5 +1,6 @@
 import { Locale } from '@/src/types';
 import { getPopularMovies } from './lib/getPopularMovies';
+import { getPopularSeries } from './lib/getPopularSeries';
 
 interface Props {
   params: {
@@ -9,12 +10,20 @@ interface Props {
 
 const SearchPage: React.FC<Props> = async ({ params: { lang } }) => {
   const [moviesErr, movies] = await getPopularMovies(lang);
+  const [seriesErr, series] = await getPopularSeries(lang);
+  console.log(series);
 
   return (
     <>
-      <h2 className='category-subtitle'>Popular</h2>
+      <h2 className='category-subtitle'>Popular movies</h2>
       {movies?.map((movie: any) => (
         <div key={movie.id}>{movie.title}</div>
+      ))}
+
+      <h2 className='category-subtitle'>Popular Series</h2>
+
+      {series?.map((show: any) => (
+        <div key={show.id}>{show.name}</div>
       ))}
     </>
   );
