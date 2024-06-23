b8281eaabfd14a373b0da6dda0aa5724ede66c6b
LugpDev2022 <lugpdev2022@gmail.com>
2024-06-22 18:38:27 -0600

    Show popular slider image on an img instead of a background

--- src/app/[lang]/(protected)/(explore)/components/PopularSlider.tsx
+++ src/app/[lang]/(protected)/(explore)/components/PopularSlider.tsx
@@ -18,11 +18,16 @@ const PopularSlider: React.FC<Props> = ({ data }) => {
         <ul>
           {data.map((item: any) => {
             return (
-              <li
-                key={item.id}
-                className={`popular-slider-item bg-[url("https://image.tmdb.org/t/p/original${item.backdrop_path}")]`}
-              >
-                <div className='bg-gradient-to-b from-black/80 via-black/50 to-black/80 w-full h-full absolute top-0 left-0 z-0'></div>
+              <li key={item.id} className='popular-slider-item'>
+                <div className='w-full h-full absolute top-0 left-0'>
+                  <div className='bg-gradient-to-b from-black/80 via-black/50 to-black/80 w-full h-full absolute top-0 left-0'></div>
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img
+                    src={`https://image.tmdb.org/t/p/original${item.backdrop_path}`}
+                    alt='Poster'
+                  />
+                </div>
+
                 <div className='popular-slider-info-container'>
                   <h2>Inside Out 2</h2>
                   <time dateTime='2024'>2024</time>
