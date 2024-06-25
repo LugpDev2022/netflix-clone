dffad57f441176c5cda93a1c9b191487bc5fb64c
LugpDev2022 <lugpdev2022@gmail.com>
2024-06-24 21:35:33 -0600

    Make the carousel arrow controls work

--- src/app/[lang]/(protected)/(explore)/components/Carousel/index.tsx
+++ src/app/[lang]/(protected)/(explore)/components/Carousel/index.tsx
@@ -1,16 +1,47 @@
+'use client';
+
+import { useRef } from 'react';
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
 import styles from './carousel.module.css';
 
 const Carousel = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const slidesContainerRef = useRef<HTMLDivElement>(null);
+
+  const scrollLeft = () => {
+    if (!containerRef.current || !slidesContainerRef.current) return;
+
+    const containerWidth = containerRef.current.clientWidth;
+
+    slidesContainerRef.current.scrollBy({
+      left: -containerWidth,
+      behavior: 'smooth',
+    });
+  };
+
+  const scrollRight = () => {
+    if (!containerRef.current || !slidesContainerRef.current) return;
+
+    const containerWidth = containerRef.current.clientWidth;
+
+    slidesContainerRef.current.scrollBy({
+      left: containerWidth,
+      behavior: 'smooth',
+    });
+  };
+
   return (
-    <div className={styles.container}>
-      <button className={styles.leftArrow}>
+    <div className={styles.container} ref={containerRef}>
+      <button className={styles.leftArrow} onClick={scrollLeft}>
         <MdKeyboardArrowLeft size={30} />
       </button>
-      <button className={styles.rightArrow}>
+      <button className={styles.rightArrow} onClick={scrollRight}>
         <MdKeyboardArrowRight size={30} />
       </button>
-      <div className={`${styles.slidesContainer} no-scrollbar`}>
+      <div
+        className={`${styles.slidesContainer} no-scrollbar`}
+        ref={slidesContainerRef}
+      >
         <div className='h-[250px] w-[200px] bg-gray-300 shrink-0'></div>
         <div className='h-[250px] w-[200px] bg-gray-300 shrink-0'></div>
         <div className='h-[250px] w-[200px] bg-gray-300 shrink-0'></div>
