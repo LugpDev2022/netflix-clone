e72b58caffcee6ca8b70ee2e6d7bce66822dd60c
LugpDev2022 <lugpdev2022@gmail.com>
2024-06-26 13:12:35 -0600

    Parse getSeriesByName response

--- src/app/[lang]/(protected)/lib/getSeriesByName.ts
+++ src/app/[lang]/(protected)/lib/getSeriesByName.ts
@@ -1,15 +1,30 @@
-import { Locale } from '@/src/types';
+import { Locale, TMDBData } from '@/src/types';
 
 export const getSeriesByName = async (
   query: string,
   lang: Locale
-): Promise<[Error?, []?]> => {
+): Promise<[Error?, TMDBData[]?]> => {
   try {
     const resp = await fetch(
       `https://api.themoviedb.org/3/search/tv?api_key=${process.env.TMDB_API_KEY}&language=${lang}&query=${query}`
     );
 
-    const { results } = await resp.json();
+    const { results }: { results: any[] } = await resp.json();
+
+    // Filter the results that don't have a backdrop path
+    const filteredResults = results.filter(
+      ({ backdrop_path }) => backdrop_path
+    );
+
+    const parsedResults: TMDBData[] = filteredResults.map(
+      ({ backdrop_path, id, poster_path, first_air_date, name }) => ({
+        id,
+        title: name,
+        poster_path,
+        release_date: first_air_date,
+        backdrop_path,
+      })
+    );
 
     return [undefined, results];
   } catch (error) {
