a803d3432e6efc4820322d48556ec4863b5a71d4
LugpDev2022 <lugpdev2022@gmail.com>
2024-06-29 22:33:50 -0600

    Improve error handling on getAllPopular

--- src/app/[lang]/(protected)/lib/getAllPopular.ts
+++ src/app/[lang]/(protected)/lib/getAllPopular.ts
@@ -6,17 +6,24 @@ export const getAllPopular = async (
 ): Promise<[Error?, TMDBData[]?]> => {
   try {
     const resp = await fetch(
-      `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.TMDB_API_KEY}&language=${lang}`
+      `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.TMDB_API_KEY}&language=${lang}`,
+      { next: { revalidate: 3600 } }
     );
 
-    const { results }: { results: [] } = await resp.json();
+    if (!resp.ok) throw new Error(resp.statusText);
 
-    const parsedResults = parseResults('default', results);
+    const data = await resp.json();
+
+    if (!data.results) {
+      throw new Error('Invalid data format received from the API');
+    }
+
+    const parsedResults = parseResults('default', data.results);
 
     return [undefined, parsedResults];
   } catch (error) {
     if (error instanceof Error) return [error];
   }
 
-  return [new Error('Unkown error')];
+  return [new Error('Unknown error')];
 };
