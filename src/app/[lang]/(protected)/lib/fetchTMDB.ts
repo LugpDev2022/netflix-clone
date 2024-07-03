86266c26453b4e7c2affe86ce28c008b0504a4ee
LugpDev2022 <lugpdev2022@gmail.com>
2024-07-03 11:55:43 -0600

    Create the fetchTMDB.ts

---
+++ src/app/[lang]/(protected)/lib/fetchTMDB.ts
@@ -0,0 +1,33 @@
+import { parseResults } from '@/src/app/[lang]/(protected)/lib/parseResults';
+import { Locale, TMDBData } from '@/src/types';
+
+export const fetchTMDB = async (
+  lang: Locale,
+  endpoint: string,
+  type: 'movie' | 'tv'
+): Promise<[Error?, TMDBData[]?]> => {
+  try {
+    const resp = await fetch(
+      `https://api.themoviedb.org/3${endpoint}?api_key=${process.env.TMDB_API_KEY}&language=${lang}`,
+      {
+        next: {
+          revalidate: 3600,
+        },
+      }
+    );
+
+    const { results }: { results: [] } = await resp.json();
+
+    const parsedResults = parseResults(type, results);
+
+    console.log(parsedResults);
+
+    return [undefined, parsedResults];
+  } catch (error) {
+    if (error instanceof Error) {
+      return [error];
+    }
+  }
+
+  return [new Error('Unknown error')];
+};
