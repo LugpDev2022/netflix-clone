f3cc756c722030253cad4ce2b445b4f000df8ecc
LugpDev2022 <lugpdev2022@gmail.com>
2024-07-09 12:13:25 -0600

    Translate the home page

--- src/app/[lang]/(protected)/(browse)/(home)/page.tsx
+++ src/app/[lang]/(protected)/(browse)/(home)/page.tsx
@@ -2,6 +2,7 @@ import Section from '@/src/app/[lang]/(protected)/(browse)/components/Section';
 import { fetchTMDB } from '@/src/app/[lang]/(protected)/lib/fetchTMDB';
 import { getAllPopular } from '@/src/app/[lang]/(protected)/lib/getAllPopular';
 import { Locale } from '@/src/types';
+import { getDictionary } from '../../../dictionaries';
 
 interface Props {
   params: {
@@ -10,30 +11,34 @@ interface Props {
 }
 
 const HomePage: React.FC<Props> = async ({ params: { lang } }) => {
-  //TODO: Translate sections titles
+  const dict = await getDictionary(lang);
 
   return (
     <>
-      <Section fetchDataFn={getAllPopular} lang={lang} title='Popular' />
+      <Section
+        fetchDataFn={getAllPopular}
+        lang={lang}
+        title={dict.app.browse.sections.home.popular}
+      />
       <Section
         fetchDataFn={() => fetchTMDB(lang, '/movie/top_rated', 'movie')}
         lang={lang}
-        title='Top Rated Movies'
+        title={dict.app.browse.sections.movies.topRated}
       />
       <Section
         fetchDataFn={() => fetchTMDB(lang, '/movie/now_playing', 'movie')}
         lang={lang}
-        title='Now Playing'
+        title={dict.app.browse.sections.movies.upcoming}
       />
       <Section
         fetchDataFn={() => fetchTMDB(lang, '/tv/top_rated', 'tv')}
         lang={lang}
-        title='Top Rated Series'
+        title={dict.app.browse.sections.series.topRated}
       />
       <Section
         fetchDataFn={() => fetchTMDB(lang, '/tv/on_the_air', 'tv')}
         lang={lang}
-        title='On Air Series'
+        title={dict.app.browse.sections.series.onAir}
       />
     </>
   );
